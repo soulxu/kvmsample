78e7f70d9663f44a006ce2d90df063d76b8bade8
BillXiang <xiangwencheng@gmail.com>
2024-06-06 17:13:58 +0800

    success

--- kvmsample-rust/src/main.rs
+++ kvmsample-rust/src/main.rs
@@ -5,7 +5,6 @@ extern crate libc;
 use std::{ffi::CString, fs::File, io::{Read, Write}, ptr::null_mut, slice};
 use kvm_bindings::{kvm_userspace_memory_region, KVM_MEM_LOG_DIRTY_PAGES};
 use kvm_ioctls::{Kvm, VcpuExit, VcpuFd, VmFd};
-// use libc;
 
 const RAM_SIZE:i32 =  512000000;
 const CODE_START:u16 = 0x1000;
@@ -13,19 +12,11 @@ const BASE:u64 = 0x1000 * 16;
 const RFLAGS:u64 = 0x0000000000000002;
 const RSP:u64 = 0xffffffff;
 
-struct MyKvm {
-    kvm: Kvm,
-    kvm_version: i32,
-    vm_fd: VmFd,
-    vcpu_fd: VcpuFd,
-    ram_size: u64,
-    ram_start: *mut u8,
-    mem: kvm_userspace_memory_region,
-}
+struct MyKvm {}
 
 impl MyKvm {
-    fn kvm_reset_vcpu(&self) {
-        let mut sregs= self.vcpu_fd.get_sregs().unwrap();
+    fn kvm_reset_vcpu(vcpu_fd: &VcpuFd) {
+        let mut sregs= vcpu_fd.get_sregs().unwrap();
         sregs.cs.selector = CODE_START;
         sregs.cs.base = BASE;
         sregs.ss.selector = CODE_START;
@@ -39,18 +30,18 @@ impl MyKvm {
         sregs.gs.selector = CODE_START;
         
 
-        self.vcpu_fd.set_sregs(&sregs);
+        vcpu_fd.set_sregs(&sregs).unwrap();
 
-        let mut regs =  self.vcpu_fd.get_regs().unwrap();
+        let mut regs =  vcpu_fd.get_regs().unwrap();
         regs.rflags = RFLAGS;
         regs.rip = 0;
         regs.rsp = RSP;
         regs.rbp = 0;
 
-        self.vcpu_fd.set_regs(&regs);
+        vcpu_fd.set_regs(&regs).unwrap();
     }
 
-    fn load_binary(&self) {
+    fn load_binary(ram_start: *mut u8) {
         const BINARY_FILE:&str = "test.bin";
 
         match File::open(BINARY_FILE) {
@@ -59,7 +50,7 @@ impl MyKvm {
                 let mut buffer: Vec<u8> = Vec::new();
                 let len = file.read_to_end(&mut buffer).unwrap();
                 unsafe {
-                    let mut slice = slice::from_raw_parts_mut(self.ram_start, len as usize);
+                    let mut slice = slice::from_raw_parts_mut(ram_start, len as usize);
                     slice.write(&buffer[..]).unwrap();
                 }
             },
@@ -67,19 +58,14 @@ impl MyKvm {
         }
     }
 
-    fn kvm_init() -> MyKvm {
+    fn kvm_init() -> Kvm {
         const KVM_DEVICE:&str = "/dev/kvm";
         let kvm_path = CString::new(KVM_DEVICE).unwrap();
-        let mut my_kvm = MyKvm {
-            kvm: Kvm::new_with_path(&kvm_path).unwrap(),
-            ..Default::default()
-        };
-        my_kvm.kvm_version = Kvm::get_api_version(&my_kvm.kvm);
-        my_kvm
+        Kvm::new_with_path(&kvm_path).unwrap()
     }
 
-    fn kvm_create_vm(&mut self, ram_size: i32) -> i32 {
-        let vm_fd = Kvm::create_vm(&self.kvm).unwrap();
+    fn kvm_create_vm(kvm: &Kvm, ram_size: i32) -> (VmFd, *mut u8) {
+        let vm_fd = Kvm::create_vm(kvm).unwrap();
         let ram_size = ram_size as u64;
         let ram_start = unsafe {
             libc::mmap(
@@ -92,10 +78,6 @@ impl MyKvm {
             ) as *mut u8
         };
 
-        // if self.ram_start == libc::MAP_FAILED {
-        //     eprintln!("can not mmap ram");
-        //     return -1
-        // }
         let mem = kvm_userspace_memory_region {
             slot: 0,
             flags: KVM_MEM_LOG_DIRTY_PAGES,
@@ -107,18 +89,18 @@ impl MyKvm {
         unsafe {
             vm_fd.set_user_memory_region(mem).unwrap();
         }
-        0
+        (vm_fd, ram_start)
     }
 
-    fn kvm_cpu_thread(&mut self) {
-        MyKvm::kvm_reset_vcpu(self);
+    fn kvm_cpu_thread(vcpu_fd: &mut VcpuFd) {
+        MyKvm::kvm_reset_vcpu(vcpu_fd);
         loop {
-            match self.vcpu_fd.run().expect("run failed") {
+            match vcpu_fd.run().expect("run failed") {
                 VcpuExit::IoIn(addr, _data ) => {
-                    println!("KVM_EXIT_IO_IN addr{}", addr);
+                    println!("KVM_EXIT_IO_IN addr {}", addr);
                 }
-                VcpuExit::IoOut(addr, _data) => {
-                    println!("KVM_EXIT_IO_OUT addr{}", addr);
+                VcpuExit::IoOut(addr, data) => {
+                    println!("KVM_EXIT_IO_OUT addr:{} data:{}", addr, data[0]);
                 }
                 VcpuExit::Unknown => {
                     println!("KVM_EXIT_UNKNOWN");
@@ -144,20 +126,19 @@ impl MyKvm {
         }
     }
 
-    fn kvm_init_vcpu(&mut self, vcpu_id: u64) {
-        self.vcpu_fd = self.vm_fd.create_vcpu(vcpu_id).unwrap();
+    fn kvm_init_vcpu(vm_fd: VmFd, vcpu_id: u64) -> VcpuFd {
+        vm_fd.create_vcpu(vcpu_id).unwrap()
     }
 
-    fn kvm_run_vm(&mut self) {
-        self.kvm_cpu_thread();
+    fn kvm_run_vm(vcpu_fd: &mut VcpuFd) {
+        MyKvm::kvm_cpu_thread(vcpu_fd);
     }
-
 }
 
 fn main() {
-    let mut my_kvm = MyKvm::kvm_init();
-    my_kvm.kvm_create_vm(RAM_SIZE);
-    my_kvm.load_binary();
-    my_kvm.kvm_init_vcpu(0);
-    my_kvm.kvm_run_vm();
+    let kvm = MyKvm::kvm_init();
+    let (vm_fd, ram_start) = MyKvm::kvm_create_vm(&kvm, RAM_SIZE);
+    MyKvm::load_binary(ram_start);
+    let mut vcpu_fd = MyKvm::kvm_init_vcpu(vm_fd, 0);
+    MyKvm::kvm_run_vm(&mut vcpu_fd);
 }
